472bf73dbe6b5bd072a3ba7f6d4bacc6e42611d2
Kien <kienreno932@gmail.com>
2020-11-16 10:45:50 +0700

    first commit

---
+++ WebBanHang/Models/CommentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBanHang.Models
+{
+    public class CommentModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vui lòng không bỏ trống bình luận")]
+        [Display(Name = "Bình luận")]
+        public string CommentContent { get; set; }
+        public int IDProduct { get; set; }
+        [ForeignKey("IDProduct")]
+        public Product Product { get; set; }
+    }
+}
---
+++ WebBanHang/Models/SupplierModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBanHang.Models
+{
+    public class SupplierModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vui lòng không để trống tên nhà cung cấp")]
+        [Display(Name = "Tên nhà cung cấp")]
+        public string Name { get; set; }
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Vui lòng nhập Email hợp lệ")]
+        public string Email { get; set; }
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Vui lòng nhập số điện thoại hợp lệ")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Vui lòng không để trống địa chỉ nhà sản xuất")]
+        [Display(Name = "Địa chỉ")]
+        public string Address { get; set; }
+    }
+}
